4114c951f87810d4a662d6e78e47c4a8fbbb94ed
polaka789 <runbysekhar2000@gmail.com>
2024-06-26 11:47:13 +0530

    secondcommit

--- src/java1/Data_type.java
+++ src/java1/Data_type.java
@@ -4,8 +4,27 @@ public class Data_type {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=10;
-System.out.println(a);
+		 //Numerical data types
+		byte a=127;
+     	short b=3276;
+     	int c=12345432;
+     	long d=234334;
+
+//Decimal data types
+     	
+     	float e=10.44f;
+     	double f=122.999;
+     	
+     	//Boolean data types
+     	
+     	boolean g=true;
+     	boolean h=false;
+     	
+     	///charcter data types
+     	
+     	char i='s';
+
+
 	}
 
 }
