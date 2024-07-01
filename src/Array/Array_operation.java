294886307a34bae8285cee22ec6f12b6a16f3a7d
lakshmikanth23 <lakshmikanth.gudise@gmail.com>
2024-07-01 18:33:14 +0530

    Task2 commit

---
+++ src/Array/Array_operation.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Array_operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int []arr= {1,2,3,4,5,6,7};
+		
+		String[]str= {"Saikiran","Srujan","laxhmi","Hemanth"};
+		
+	//Iterating
+		for(String str1:str) {
+			System.out.println(str1);
+		}
+		System.out.println(str[0]);
+
+	}
+
+}
---
+++ src/Task/swap.java
@@ -0,0 +1,17 @@
+package Task;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int temp;
+System.out.println("before swap "+a+" "+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("after swap "+a+" "+b);
+	}
+
+}
